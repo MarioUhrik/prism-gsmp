MarioUhrik/prism-gsmp

commit 2eb952891ab839dadee3078517a3f1fb35301c8a

ACTMCPotatoX_poly:
Fixed an off-by-one error in all ACTMCPotato polynomial implementations.
However, this will have no visible impact on the user.

prism/src/explicit/ACTMCPotatoErlang_poly.java
@@ -139,7 +139,7 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 			// Also, initialize the polynomials.
 			for (int i = 0; i < numStates; i++) {
 				soln[i] = 0;
-				polynomials[i] = new Polynomial(BigDecimal.ZERO);
+				polynomials[i] = new Polynomial();
 			}
 			soln[ACTMCtoDTMC.get(entrance)] = 1;
 
@@ -148,13 +148,13 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 			if (left == 0) {
 				for (int i = 0; i < numStates; i++) {
 					polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-					for (int j = left + 1; j <= right; ++j) {
+					for (int j = 1; j <= right; ++j) {
 						polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 					}
 				}
 			} else {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = 0; j <= left; ++j) {
+					for (int j = 0; j < left; ++j) {
 						polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 					}
 					for (int j = left; j <= right; ++j) {
@@ -175,7 +175,7 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 				// Add to sum
 				if (iters >= left) {
 					for (int i = 0; i < numStates; i++) {
-						for (int j = iters + 1; j <= right; ++j) {
+						for (int j = iters + 1; j < right; ++j) {
 							BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 							polynomials[i].coeffs.set(j, tmp);
 						}
@@ -270,8 +270,8 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 			// Initialize the arrays
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
-				polynomialsBeforeEvent[i] = new Polynomial(BigDecimal.ZERO);
-				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
+				polynomialsBeforeEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial();
 			}
 
 			// If necessary, compute the 0th element of summation
@@ -423,7 +423,7 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 			} else {
 				soln[s] = 0;
 			}
-			polynomials[s] = new Polynomial(BigDecimal.ZERO);
+			polynomials[s] = new Polynomial();
 		}
 
 		// do 0th element of summation (doesn't require any matrix powers), and initialize the coefficients
@@ -431,13 +431,13 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 		if (left == 0) {
 			for (int i = 0; i < numStates; i++) {
 				polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-				for (int j = left + 1; j <= right; ++j) {
+				for (int j = 1; j <= right; ++j) {
 					polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 				}
 			}
 		} else {
 			for (int i = 0; i < numStates; i++) {
-				for (int j = 0; j <= left; ++j) {
+				for (int j = 0; j < left; ++j) {
 					polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 				}
 				for (int j = left; j <= right; ++j) {
@@ -458,7 +458,7 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 			// Add to sum
 			if (iters >= left) {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = iters + 1; j <= right; ++j) {
+					for (int j = iters + 1; j < right; ++j) {
 						BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 						polynomials[i].coeffs.set(j, tmp);
 					}
@@ -539,7 +539,7 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato
 		}*/
 		
 		Polynomial poly = new Polynomial(new ArrayList<BigDecimal>(polynomial.coeffs));
-		Polynomial antiderivative = new Polynomial(BigDecimal.ZERO);
+		Polynomial antiderivative = new Polynomial();
 		BigDecimal factor = BigDecimal.ONE.negate().divide
 				(new BigDecimal(String.valueOf(uniformizationRate), mc).add(new BigDecimal(String.valueOf(event.getFirstParameter()),mc), mc), mc);
 		

prism/src/explicit/ACTMCPotatoUniform_poly.java
@@ -165,14 +165,14 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 				for (int i = 0; i < numStates; i++) {
 					dirac.getMeanTimes();
 					soln[i] = dirac.meanTimesSoln.get(entrance).get(DTMCtoACTMC.get(i));
-					polynomials[i] = new Polynomial(BigDecimal.ZERO);
+					polynomials[i] = new Polynomial();
 				}
 			} else {
 				// Initialize the solution array by assigning reward 1 to the entrance and 0 to all others.
 				// Also, initialize the polynomials.
 				for (int i = 0; i < numStates; i++) {
 					soln[i] = 0;
-					polynomials[i] = new Polynomial(BigDecimal.ZERO);
+					polynomials[i] = new Polynomial();
 				}
 				soln[ACTMCtoDTMC.get(entrance)] = 1;
 			}
@@ -182,13 +182,13 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 			if (left == 0) {
 				for (int i = 0; i < numStates; i++) {
 					polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-					for (int j = left + 1; j <= right; ++j) {
+					for (int j = 1; j <= right; ++j) {
 						polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 					}
 				}
 			} else {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = 0; j <= left; ++j) {
+					for (int j = 0; j < left; ++j) {
 						polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 					}
 					for (int j = left; j <= right; ++j) {
@@ -209,7 +209,7 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 				// Add to sum
 				if (iters >= left) {
 					for (int i = 0; i < numStates; i++) {
-						for (int j = iters + 1; j <= right; ++j) {
+						for (int j = iters + 1; j < right; ++j) {
 							BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 							polynomials[i].coeffs.set(j, tmp);
 						}
@@ -308,8 +308,8 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 			// Initialize the arrays
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
-				polynomialsBeforeEvent[i] = new Polynomial(BigDecimal.ZERO);
-				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
+				polynomialsBeforeEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial();
 			}
 
 			// If necessary, compute the 0th element of summation
@@ -450,7 +450,7 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 			for (int i = 0; i < numStates; i++) {
 				dirac.getMeanRewards();
 				soln[i] = dirac.meanRewardsSoln.get(DTMCtoACTMC.get(i));
-				polynomials[i] = new Polynomial(BigDecimal.ZERO);
+				polynomials[i] = new Polynomial();
 			}
 		} else {
 			// Initialize the solution array by assigning rewards to the potato states
@@ -463,7 +463,7 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 				} else {
 					soln[i] = 0;
 				}
-				polynomials[i] = new Polynomial(BigDecimal.ZERO);
+				polynomials[i] = new Polynomial();
 			}
 		}
 
@@ -472,13 +472,13 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 		if (left == 0) {
 			for (int i = 0; i < numStates; i++) {
 				polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-				for (int j = left + 1; j <= right; ++j) {
+				for (int j = 1; j <= right; ++j) {
 					polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 				}
 			}
 		} else {
 			for (int i = 0; i < numStates; i++) {
-				for (int j = 0; j <= left; ++j) {
+				for (int j = 0; j < left; ++j) {
 					polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 				}
 				for (int j = left; j <= right; ++j) {
@@ -499,7 +499,7 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 			// Add to sum
 			if (iters >= left) {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = iters + 1; j <= right; ++j) {
+					for (int j = iters + 1; j < right; ++j) {
 						BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 						polynomials[i].coeffs.set(j, tmp);
 					}
@@ -577,7 +577,7 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato
 		}*/
 		
 		Polynomial poly = new Polynomial(new ArrayList<BigDecimal>(polynomial.coeffs));
-		Polynomial antiderivative = new Polynomial(BigDecimal.ZERO);
+		Polynomial antiderivative = new Polynomial();
 		BigDecimal factor = BigDecimal.ONE.negate().divide(new BigDecimal(String.valueOf(uniformizationRate), mc), mc);
 		
 		int polyDegree = poly.degree();

prism/src/explicit/ACTMCPotatoWeibull_polyTaylor.java
@@ -147,7 +147,7 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 			// Also, initialize the polynomials.
 			for (int i = 0; i < numStates; i++) {
 				soln[i] = 0;
-				polynomials[i] = new Polynomial(BigDecimal.ZERO);
+				polynomials[i] = new Polynomial();
 			}
 			soln[ACTMCtoDTMC.get(entrance)] = 1;
 
@@ -156,13 +156,13 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 			if (left == 0) {
 				for (int i = 0; i < numStates; i++) {
 					polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-					for (int j = left + 1; j <= right; ++j) {
+					for (int j = 1; j <= right; ++j) {
 						polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 					}
 				}
 			} else {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = 0; j <= left; ++j) {
+					for (int j = 0; j < left; ++j) {
 						polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 					}
 					for (int j = left; j <= right; ++j) {
@@ -183,7 +183,7 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 				// Add to sum
 				if (iters >= left) {
 					for (int i = 0; i < numStates; i++) {
-						for (int j = iters + 1; j <= right; ++j) {
+						for (int j = iters + 1; j < right; ++j) {
 							BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 							polynomials[i].coeffs.set(j, tmp);
 						}
@@ -289,8 +289,8 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 			// Initialize the arrays
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
-				polynomialsBeforeEvent[i] = new Polynomial(BigDecimal.ZERO);
-				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
+				polynomialsBeforeEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial();
 			}
 
 			// If necessary, compute the 0th element of summation
@@ -453,7 +453,7 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 			} else {
 				soln[s] = 0;
 			}
-			polynomials[s] = new Polynomial(BigDecimal.ZERO);
+			polynomials[s] = new Polynomial();
 		}
 
 		// do 0th element of summation (doesn't require any matrix powers), and initialize the coefficients
@@ -461,13 +461,13 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 		if (left == 0) {
 			for (int i = 0; i < numStates; i++) {
 				polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-				for (int j = left + 1; j <= right; ++j) {
+				for (int j = 1; j <= right; ++j) {
 					polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 				}
 			}
 		} else {
 			for (int i = 0; i < numStates; i++) {
-				for (int j = 0; j <= left; ++j) {
+				for (int j = 0; j < left; ++j) {
 					polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 				}
 				for (int j = left; j <= right; ++j) {
@@ -488,7 +488,7 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato
 			// Add to sum
 			if (iters >= left) {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = iters + 1; j <= right; ++j) {
+					for (int j = iters + 1; j < right; ++j) {
 						BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 						polynomials[i].coeffs.set(j, tmp);
 					}

prism/src/explicit/ACTMCSymbolicParameterSynthesis.java
@@ -171,7 +171,7 @@ public class ACTMCSymbolicParameterSynthesis extends ACTMCReduction
 			
 			switch (event.getDistributionType().getEnum()) { //Parameter synthesis requires the "poly" implementations!
 			case DIRAC:
-				potatoData = new ACTMCPotatoDirac_poly(actmc, event, rew, target);
+				potatoData = new ACTMCPotatoDirac_polyTaylor(actmc, event, rew, target);
 			case ERLANG:
 				potatoData = new ACTMCPotatoErlang_poly(actmc, event, rew, target);
 				break;

prism/src/common/polynomials/Polynomial.java
@@ -54,6 +54,14 @@ public class Polynomial {
 	private Polynomial derivative;
 	
 	/**
+	 * Constructor for an empty polynomial
+	 */
+	public Polynomial(){
+		List<BigDecimal> newCoeffs = new ArrayList<>();
+		this.coeffs = newCoeffs;
+	}
+	
+	/**
 	 * Constructor with list of coefficients
 	 * @param	coeffs	coefficients list
 	 */

prism/src/explicit/ACTMCPotatoDirac_poly.java
@@ -133,7 +133,7 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 			// Also, initialize the polynomials.
 			for (int i = 0; i < numStates; i++) {
 				soln[i] = 0;
-				polynomials[i] = new Polynomial(BigDecimal.ZERO);
+				polynomials[i] = new Polynomial();
 			}
 			soln[ACTMCtoDTMC.get(entrance)] = 1;
 
@@ -143,13 +143,13 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 			if (left == 0) {
 				for (int i = 0; i < numStates; i++) {
 					polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-					for (int j = left + 1; j <= right; ++j) {
+					for (int j = 1; j <= right; ++j) {
 						polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 					}
 				}
 			} else {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = 0; j <= left; ++j) {
+					for (int j = 0; j < left; ++j) {
 						polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 					}
 					for (int j = left; j <= right; ++j) {
@@ -170,7 +170,7 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 				// Add to sum
 				if (iters >= left) {
 					for (int i = 0; i < numStates; i++) {
-						for (int j = iters + 1; j <= right; ++j) {
+						for (int j = iters + 1; j < right; ++j) {
 							BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 							polynomials[i].coeffs.set(j, tmp);
 						}
@@ -258,8 +258,8 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 			// Initialize the arrays
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
-				polynomialsBeforeEvent[i] = new Polynomial(BigDecimal.ZERO);
-				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
+				polynomialsBeforeEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial();
 			}
 
 			// If necessary, compute the 0th element of summation
@@ -401,7 +401,7 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 			} else {
 				soln[i] = 0;
 			}
-			polynomials[i] = new Polynomial(BigDecimal.ZERO);
+			polynomials[i] = new Polynomial();
 		}
 
 		// do 0th element of summation (doesn't require any matrix powers), and initialize the coefficients
@@ -409,13 +409,13 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 		if (left == 0) {
 			for (int i = 0; i < numStates; i++) {
 				polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-				for (int j = left + 1; j <= right; ++j) {
+				for (int j = 1; j <= right; ++j) {
 					polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 				}
 			}
 		} else {
 			for (int i = 0; i < numStates; i++) {
-				for (int j = 0; j <= left; ++j) {
+				for (int j = 0; j < left; ++j) {
 					polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 				}
 				for (int j = left; j <= right; ++j) {
@@ -436,7 +436,7 @@ public class ACTMCPotatoDirac_poly extends ACTMCPotato
 			// Add to sum
 			if (iters >= left) {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = iters + 1; j <= right; ++j) {
+					for (int j = iters + 1; j < right; ++j) {
 						BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 						polynomials[i].coeffs.set(j, tmp);
 					}

prism/src/explicit/ACTMCPotatoDirac_polyTaylor.java
@@ -136,7 +136,7 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 			// Also, initialize the polynomials.
 			for (int i = 0; i < numStates; i++) {
 				soln[i] = 0;
-				polynomials[i] = new Polynomial(BigDecimal.ZERO);
+				polynomials[i] = new Polynomial();
 			}
 			soln[ACTMCtoDTMC.get(entrance)] = 1;
 
@@ -146,13 +146,13 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 			if (left == 0) {
 				for (int i = 0; i < numStates; i++) {
 					polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-					for (int j = left + 1; j <= right; ++j) {
+					for (int j = 1; j <= right; ++j) {
 						polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 					}
 				}
 			} else {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = 0; j <= left; ++j) {
+					for (int j = 0; j < left; ++j) {
 						polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 					}
 					for (int j = left; j <= right; ++j) {
@@ -173,7 +173,7 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 				// Add to sum
 				if (iters >= left) {
 					for (int i = 0; i < numStates; i++) {
-						for (int j = iters + 1; j <= right; ++j) {
+						for (int j = iters + 1; j < right; ++j) {
 							BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 							polynomials[i].coeffs.set(j, tmp);
 						}
@@ -265,8 +265,8 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 			// Initialize the arrays
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
-				polynomialsBeforeEvent[i] = new Polynomial(BigDecimal.ZERO);
-				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
+				polynomialsBeforeEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial();
 			}
 
 			// If necessary, compute the 0th element of summation
@@ -411,7 +411,7 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 			} else {
 				soln[i] = 0;
 			}
-			polynomials[i] = new Polynomial(BigDecimal.ZERO);
+			polynomials[i] = new Polynomial();
 		}
 
 		// do 0th element of summation (doesn't require any matrix powers), and initialize the coefficients
@@ -419,13 +419,13 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 		if (left == 0) {
 			for (int i = 0; i < numStates; i++) {
 				polynomials[i].coeffs.add(left, BigDecimal.ZERO);
-				for (int j = left + 1; j <= right; ++j) {
+				for (int j = 1; j <= right; ++j) {
 					polynomials[i].coeffs.add(j, new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc));
 				}
 			}
 		} else {
 			for (int i = 0; i < numStates; i++) {
-				for (int j = 0; j <= left; ++j) {
+				for (int j = 0; j < left; ++j) {
 					polynomials[i].coeffs.add(j, BigDecimal.ZERO);
 				}
 				for (int j = left; j <= right; ++j) {
@@ -446,7 +446,7 @@ public class ACTMCPotatoDirac_polyTaylor extends ACTMCPotato
 			// Add to sum
 			if (iters >= left) {
 				for (int i = 0; i < numStates; i++) {
-					for (int j = iters + 1; j <= right; ++j) {
+					for (int j = iters + 1; j < right; ++j) {
 						BigDecimal tmp = polynomials[i].coeffs.get(j).add(new BigDecimal(soln[i], mc).multiply(weights_BD[j - left], mc), mc);
 						polynomials[i].coeffs.set(j, tmp);
 					}

commit 8b736c215519930c8bc54439010a8122708007e6

Hotfixed a recently introduced critical bug
Commit 2eb952891ab839dadee3078517a3f1fb35301c8a has introduced a bug
that caused an arrayOutOfBounds exception to be thrown during model
checking of ACTMCs with Erlang or uniform events. It should be peachy
now.

prism/src/explicit/ACTMCPotatoErlang_poly.java
@@ -276,7 +276,7 @@ public class ACTMCPotatoErlang_poly extends ACTMCPotato_poly
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
 				polynomialsBeforeEvent[i] = new Polynomial();
-				polynomialsAfterEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
 			}
 
 			// If necessary, compute the 0th element of summation

prism/src/explicit/ACTMCPotatoUniform_poly.java
@@ -314,7 +314,7 @@ public class ACTMCPotatoUniform_poly extends ACTMCPotato_poly
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
 				polynomialsBeforeEvent[i] = new Polynomial();
-				polynomialsAfterEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
 			}
 
 			// If necessary, compute the 0th element of summation

prism/src/explicit/ACTMCPotatoWeibull_polyTaylor.java
@@ -294,7 +294,7 @@ public class ACTMCPotatoWeibull_polyTaylor extends ACTMCPotato_poly
 			for (int i = 0; i < numStates; i++) {
 				result[i] = 0.0;
 				polynomialsBeforeEvent[i] = new Polynomial();
-				polynomialsAfterEvent[i] = new Polynomial();
+				polynomialsAfterEvent[i] = new Polynomial(BigDecimal.ZERO);
 			}
 
 			// If necessary, compute the 0th element of summation

prism/src/explicit/ACTMCReduction.java
@@ -76,8 +76,8 @@ public class ACTMCReduction extends PrismComponent
 	 *  2) or it is an entrance to some potato. */
 	protected BitSet relevantStates;
 	/** Map where the keys are string identifiers of the GSMPEvents,
-	 *  and the values are corresponding ACTMCPotatoData structures.
-	 *  This is useful for fast access and efficient reusage of the ACTMCPotatoData structures.*/
+	 *  and the values are corresponding ACTMCPotato structures.
+	 *  This is useful for fast access and efficient reusage of the ACTMCPotato structures.*/
 	protected Map<String, ACTMCPotato> pdMap;
 	/** DTMC equivalent to {@code actmc} eventually generated by this class.
 	 *  Initially null.*/

prism/src/explicit/ACTMCSymbolicParameterSynthesis.java
@@ -49,8 +49,8 @@ public class ACTMCSymbolicParameterSynthesis extends ACTMCReduction
 {
 	
 	/** Map where the keys are string identifiers of the GSMPEvents,
-	 *  and the values are corresponding ACTMCPotatoData_poly structures.
-	 *  This is useful for fast access and efficient reusage of the ACTMCPotatoData structures.*/
+	 *  and the values are corresponding ACTMCPotato_poly structures.
+	 *  This is useful for fast access and efficient reusage of the ACTMCPotato_poly structures.*/
 	protected Map<String, ACTMCPotato_poly> polyPDMap;
 	/** True if we are minimizing the rewards. Otherwise, if maximizing, this variable is false. */
 	protected boolean min;